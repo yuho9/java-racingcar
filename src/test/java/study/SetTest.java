768f308a6ad2d836447b9a36a123efe8cb5e56a6
yuho9 <catch919@gmail.com>
2024-06-30 18:25:04 +0900

    요구사항 2 : Set의 contains() 메소드를 활용해 1, 2, 3의 값이 존재하는지를 확인하는 학습테스트를 구현하려한다.

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -2,6 +2,7 @@ package study;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -9,6 +10,8 @@ import java.util.Set;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class SetTest {
 	private Set<Integer> numbers;
@@ -28,5 +31,11 @@ public class SetTest {
 		assertEquals(3,numbers.size());
 	}
 	
-
+	@ParameterizedTest
+	@ValueSource(ints = {1,2,3})
+	@DisplayName("요구사항 2 : Set의 contains() 메소드를 활용해 1, 2, 3의 값이 존재하는지를 확인하는 학습테스트를 구현하려한다.")
+	void checkContain(int input) {
+	assertTrue(numbers.contains(input));
+	}
+	
 }
